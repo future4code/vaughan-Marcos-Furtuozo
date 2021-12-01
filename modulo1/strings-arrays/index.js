e2043ef8f4e4d19825a5d83a0f45dabfc01192c9
marcos ney da silva <furmar2428@hotmail.com>
2021-12-01 17:26:33 -0300

    primeira questão

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,8 @@
+// Crie um programa que peça ao usuário para inserir o seu nome e sua cor favorita e 
+//imprima a mensagem: 
+const nomeDoUsuario = prompt("digite seu nome");
+const emailDoUsuario = prompt("digite email");
+console.log(nomeDoUsuario)
+console.log(emailDoUsuario)
+// Faça o exercício duas vezes, utilizando template strings e concatenação
+
