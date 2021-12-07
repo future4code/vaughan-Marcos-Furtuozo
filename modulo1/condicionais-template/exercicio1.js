447bf3f457953daf697b68a809a3f4da896216f5
marcos ney da silva <furmar2428@hotmail.com>
2021-12-07 19:52:45 -0300

    Create exercicio1.js

---
+++ modulo1/condicionais-template/exercicio1.js
@@ -0,0 +1,13 @@
+//Crie uma função que recebe 2 números (chamaremos de num1 e num2)
+//Compara esses números entre si
+//Se os números forem iguais, retorna uma mensagem de sucesso
+
+function compareNumbers (n1, n2) {
+// insira o código aqui
+if(n1==n2){
+    console.log("sucesso")
+}
+}
+compareNumbers(5,5)
+
+
